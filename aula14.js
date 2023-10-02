c24938f6022765e3801a6355df7c3806ab64b113
Zélia vieira <143646144+Zeliavieira@users.noreply.github.com>
2023-10-02 13:37:51 -0300

    Add files via upload

---
+++ aula14.js
@@ -0,0 +1,28 @@
+
+// var hamburguer = document.getElementById('hamburguer');
+// var itensMenu = document.getElementById('itens_menu');
+
+
+// hamburguer.addEventListener('click', aparecer);
+
+
+// function aparecer() {
+//     if (itens_menu.style.display == 'block') {
+//         itensMenu.classList.remove('show');
+//     } else {
+//         itensMenu.classList.toggle('show');
+//     }
+// }
+
+var hamburguer = document.getElementById('hamburguer');
+var itensMenu = document.getElementById('itens_menu');
+
+hamburguer.addEventListener('click', aparecer);
+
+function aparecer() {
+    if (itens_menu.style.display == 'block') {
+        itensMenu.classList.remove('show');
+    } else {
+        itensMenu.classList.toggle('show');
+    }
+}
